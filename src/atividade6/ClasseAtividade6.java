692e2b5d1dcd7edd3faab5b6d163d6ce098ea602
abelaraujo <abel2901@gmail.com>
2018-09-13 00:57:28 -0300

    Criando inner class estatica

---
+++ src/atividade6/ClasseAtividade6.java
@@ -0,0 +1,11 @@
+package atividade6;
+
+public class ClasseAtividade6 {
+
+	static class ClasseAninhada {
+		public void metodo1() {
+			System.out.println("Classe aninhada estática");
+		}
+	}
+
+}
---
+++ src/atividade6/InnerTeste.java
@@ -0,0 +1,10 @@
+package atividade6;
+
+public class InnerTeste {
+
+	public static void main(String[] args) {
+		ClasseAtividade6.ClasseAninhada ca = new ClasseAtividade6.ClasseAninhada();
+		ca.metodo1();
+	}
+
+}
