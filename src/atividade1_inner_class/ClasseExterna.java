4710907624a4fe02ae94016e35dd9d7764c01c76
Abel <abel2901@gmail.com>
2018-09-13 14:23:38 -0300

    Update ClasseExterna.java

--- src/atividade1_inner_class/ClasseExterna.java
+++ src/atividade1_inner_class/ClasseExterna.java
@@ -4,4 +4,12 @@ public class ClasseExterna {
   
      private Integer numero;
   
+     private class ClasseInterna{
+       
+      public void imprimir(){
+          System.out.println("Dentro do método da inner class");
+      }
+       
+    }
+ 
 }
